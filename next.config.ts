344d77145e6a1ab41f2acb04864f6ed6aa1eda38
SuyashBhavalkar3 <suyashbhavalkar82@gmail.com>
2026-08-14 16:25:43 +0530

    solved only for home route

--- next.config.ts
+++ next.config.ts
@@ -21,15 +21,6 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=0, must-revalidate",
-          },
-        ],
-      },
     ];
   },
 };
